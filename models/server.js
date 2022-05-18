51e86619304bb79fbb0e29fe0acf979ce797a50b
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-05-18 15:22:13 -0300

    Setting of CORS

--- models/server.js
+++ models/server.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express'); 
+const cors = require('cors'); 
 
 class Server {
     constructor() {
@@ -10,6 +11,8 @@ class Server {
         this.routes();
     }
     middlewares(){
+        // Cors
+        this.app.use(cors())
         // Public Folder
         this.app.use(express.static('public'))
     }
