4b37a9fa1ece990eb43473aea8b9d7ce5200f065
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-05-18 12:57:42 -0300

    Initial Config of node Server

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+require('dotenv').config(); 
+
+const port = process.env.PORT; 
+const app = express(); 
+
+app.get('/',(req, res) =>{
+  res.send('Hello Mundo')
+})
+
+app.listen(port, ()=>{
+    console.log(`listen at port ${port}`);
+}); 
+
+
+
