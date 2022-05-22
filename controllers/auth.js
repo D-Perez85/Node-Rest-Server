990a31037e582b70d48b3e907c4856c44576fb5e
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2022-05-22 00:55:19 -0300

    Auth Login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,37 @@
+const bcryptjs = require("bcryptjs");
 const { response } = require("express");
-const res = require("express")
+const User = require("../models/user");
 
-const login = ( req, res=response ) =>{
-    res.json({
-        msg: 'Auth Succes'
-    })
-}
-
-module.exports = { login }; 
+const login = async (req, res = response) => {
+  const { correo, password } = req.body;
+    try {
+        // Is the email exist 
+        const user = await User.findOne({ correo });
+        if ( !user ) {
+            return res.status(400).json({
+                msg: 'User / Password are not corrects - correo'
+            });
+        }
+        // Is the user active 
+            if ( !user.estado ) {
+            return res.status(400).json({
+                msg: 'User / Password are not corrects - estado: false'
+            });
+        }
+        // Check the password
+        const validPassword = bcryptjs.compareSync( password, user.password );
+        if ( !validPassword ) {
+            return res.status(400).json({
+                msg: 'User / Password are not corrects - password'
+            });
+        }
+        res.json({
+            user
+            })
+    }catch (error) {
+        return res.status(500).json({
+        msg: "Contact the Admin",
+        });
+    }
+};
+module.exports = { login };
